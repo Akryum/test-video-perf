86c5c03c7e1c1739cb85480f4da6db6cccdec702
Guillaume Chau <guillaume.b.chau@gmail.com>
2020-09-30 12:28:16 +0200

    refactor: videos + border-radius

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,8 @@
 <script>
-import { computed, ref } from 'vue'
-import OT from '@opentok/client'
-import VideoWrapper from './VideoWrapper.vue'
-import Publisher from './Publisher.vue'
-import Subscriber from './Subscriber.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'App',
-
-  components: {
-    VideoWrapper,
-    Publisher,
-    Subscriber
-  },
 
   setup () {
     const videos = ref([
@@ -29,51 +19,13 @@ export default {
 
     // Settings
 
-    const webRtcEnabled = ref(false)
-    const overlayShown = ref(false)
-    const animationShown = ref(false)
-
-    // WebRTC
-
-    function errorHandler (err) {
-      console.error(err)
-      alert(err.message)
-    }
-
-    const streams = ref([])
-
-    const session = OT.initSession(process.env.VUE_APP_API_KEY, process.env.VUE_APP_SESSION_ID)
-    session.connect(process.env.VUE_APP_TOKEN, err => {
-      if (err) {
-        errorHandler(err)
-      }
-    })
-
-    session.on('streamCreated', event => {
-      streams.value.push(event.stream)
-    })
-
-    session.on('streamDestroyed', event => {
-      const idx = streams.value.indexOf(event.stream)
-      if (idx > -1) {
-        streams.value.splice(idx, 1)
-      }
-    })
-
-    // List
-
-    const list = computed(() => webRtcEnabled.value ? [
-      'publisher',
-      ...streams.value
-    ] : videos.value)
+    const br = ref(false)
+    const differentBr = ref(false)
 
     return {
-      list,
-      webRtcEnabled,
-      overlayShown,
-      animationShown,
-      errorHandler,
-      session
+      videos,
+      br,
+      differentBr
     }
   }
 }
@@ -83,46 +35,27 @@ export default {
   <div class="w-screen h-screen flex flex-col">
     <div class="flex-none p-4 flex space-x-6 items-center">
       <label>
-        <input type="checkbox" v-model="webRtcEnabled">
-        Use WebRTC
+        <input type="checkbox" v-model="br">
+        Border radius
       </label>
 
       <label>
-        <input type="checkbox" v-model="overlayShown">
-        Show overlay on videos
-      </label>
-
-      <label>
-        <input type="checkbox" v-model="animationShown">
-        Show animations in overlays
+        <input type="checkbox" v-model="differentBr">
+        Different border radius
       </label>
     </div>
     <div class="flex-1 grid grid-cols-3 gap-4 p-4">
-      <VideoWrapper
-        v-for="item of list"
-        :key="item"
-        :title="item.id || item"
-        :overlayShown="overlayShown"
-        :animationShown="animationShown"
+      <div
+        v-for="video of videos"
+        :key="video"
+        class="bg-gray-400 p-2 flex items-center justify-center"
       >
-        <template v-if="webRtcEnabled">
-          <Publisher
-            v-if="item === 'publisher'"
-            :session="session"
-            :opts="{
-              mirror: true
-            }"
-          />
-          <Subscriber
-            v-else
-            :stream="item"
-            :session="session"
-          />
-        </template>
-        <video v-else autoplay muted loop class="w-full h-full">
-          <source :src="item" />
+        <video autoplay muted loop class="min-w-full max-w-full max-h-full"
+          :class="br ? !differentBr ? 'rounded-lg' : 'rounded-tl rounded-tr-lg rounded-bl-sm rounded-br-md' : null"
+        >
+          <source :src="video" />
         </video>
-      </VideoWrapper>
+      </div>
     </div>
   </div>
 </template>
